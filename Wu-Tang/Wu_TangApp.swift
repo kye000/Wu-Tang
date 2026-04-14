31446f44b088a635a5cae4a3213892733147c52e
Kyle Sumagang <ksumagang2026@gmail.com>
2026-04-14 13:33:22 -0700

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by SUMAGANG, KYLE RUSSLIE on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
