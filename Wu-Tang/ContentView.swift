6705e29566d27aebf3cc0d832f9e4ab6ed0a0b10
Kyle Sumagang <ksumagang2026@gmail.com>
2026-04-14 14:11:07 -0700

    Commit #1

--- Wu-Tang/ContentView.swift
+++ Wu-Tang/ContentView.swift
@@ -8,14 +8,149 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enteredName = ""
+    @State private var staticCoderNameis = ""
+    @State private var wuTangName = ""
+    @State private var imageName = "wu-tang"
+    @FocusState private var isFocused: Bool
+    private let firstColumn = ["Algorithmic",
+                               "Byte",
+                               "Cache",
+                               "Debug",
+                               "Echo",
+                               "Function",
+                               "Git",
+                               "Hex",
+                               "Infinite",
+                               "Java",
+                               "Kernel",
+                               "Logic",
+                               "Module",
+                               "Node",
+                               "Object",
+                               "Pixel",
+                               "Query",
+                               "Runtime",
+                               "Script",
+                               "Token",
+                               "Undefined",
+                               "Virtual",
+                               "Web",
+                               "Xcode",
+                               "Yota",
+                               "Zero"]
+    private let secondColumn = ["$tack",
+                                "Processor",
+                                "Cipher",
+                                "Daemon",
+                                "EndPoint",
+                                "Framework",
+                                "Gateway",
+                                "Hub",
+                                "Interrupt",
+                                "Crash",
+                                "Loop",
+                                "Module",
+                                "Nexus",
+                                "Optimizer",
+                                "Protocol",
+                                "Queue",
+                                "Router",
+                                "Stack",
+                                "Thread",
+                                "Update",
+                                "Variable",
+                                "Widget",
+                                "Terminal",
+                                "Yield",
+                                "Zen",
+                                "Ace",
+                                "Breakpoint",
+                                "Root",
+                                "Instance",
+                                "Access",
+                                "Archive",
+                                "Control",
+                                "Justice"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Text("Wu-Tang\nCoder Name Generator")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.yellow)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .background(.black)
+            
+            Spacer()
+            
+            TextField("Enter name here", text: $enteredName)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.gray, lineWidth: 1)
+                }
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
         .padding()
+        .focused($isFocused)
+        .onChange(of: isFocused) {
+            if isFocused == true {
+                imageName = ""
+                enteredName=""
+                staticCoderNameis = ""
+            }
+        }
+        
+        Button {
+            wuTangName = getWutangName(name: enteredName)
+            isFocused = false
+            staticCoderNameis = "Your Wu-Tang Coder Name is:"
+            imageName = "wu-tang"
+        } label: {
+            Image("wu-tang-button")
+                  Text("Get It!")
+        }
+                .font(.title2)
+                .fontWeight(.bold)
+                .foregroundStyle(.yellow)
+                .buttonStyle(.borderedProminent)
+                .tint(.black)
+                .disabled(enteredName.isEmpty)
+        
+        VStack {
+            Text(staticCoderNameis)
+            Text(wuTangName)
+                .fontWeight(.black)
+        }
+        .font(.largeTitle)
+        .frame(height: 130)
+        
+        Spacer()
+        Image(imageName)
+            .resizable()
+            .scaledToFit()
+                  
+        
+    }
+    
+    func getWutangName(name: String) -> String {
+        var localname = name
+        let firstLetter = localname.removeFirst()
+        print("firstLetter = \(firstLetter)")
+        var firstColumnIndex = 0
+        for i in 0 ..< firstColumn.count {
+            if firstColumn[i].first?.uppercased() == firstLetter.uppercased() {
+                firstColumnIndex = i == 0 ? 25 : i-1
+            }
+        }
+        return "\(firstColumn[firstColumnIndex]) \(secondColumn.randomElement()!)"
     }
 }
 
